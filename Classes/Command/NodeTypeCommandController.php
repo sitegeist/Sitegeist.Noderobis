e1a1887d8bb620980b2d37c84ae81888d409c063
Martin Ficzel <ficzel@sitegeist.de>
2022-08-19 19:30:30 +0200

    TASK: Extract AbstractCommandController for beeing used by others

--- Classes/Command/NodeTypeCommandController.php
+++ Classes/Command/NodeTypeCommandController.php
@@ -8,54 +8,20 @@ declare(strict_types=1);
 
 namespace Sitegeist\Noderobis\Command;
 
-use Neos\ContentRepository\Migration\Filters\NodeName;
 use Neos\Flow\Annotations as Flow;
-use Neos\Flow\Cli\CommandController;
-use Neos\Flow\Package\Exception\UnknownPackageException;
-use Neos\Flow\Package\FlowPackageInterface;
-use Neos\Flow\Package\PackageManager;
-use Sitegeist\Noderobis\Domain\Generator\CreateFusionRendererModificationGenerator;
 use Sitegeist\Noderobis\Domain\Generator\NodeTypeGenerator;
-use Sitegeist\Noderobis\Domain\Generator\CreateNodeTypeYamlFileModificationGenerator;
-use Sitegeist\Noderobis\Domain\Modification\ModificationCollection;
-use Sitegeist\Noderobis\Domain\Modification\ModificationInterface;
 use Sitegeist\Noderobis\Domain\Specification\IconNameSpecification;
 use Sitegeist\Noderobis\Domain\Specification\NodeTypeLabelSpecification;
 use Sitegeist\Noderobis\Domain\Specification\NodeTypeNameSpecificationFactory;
-use Sitegeist\Noderobis\Domain\Specification\TetheredNodeNameSpecification;
 use Sitegeist\Noderobis\Domain\Specification\NodeTypeNameSpecification;
 use Sitegeist\Noderobis\Domain\Specification\PropertySpecificationFactory;
 use Sitegeist\Noderobis\Domain\Specification\NodeTypeNameSpecificationCollection;
 use Sitegeist\Noderobis\Domain\Specification\NodeTypeSpecification;
-use Sitegeist\Noderobis\Domain\Specification\PropertyDescriptionSpecification;
-use Sitegeist\Noderobis\Domain\Specification\PropertyGroupNameSpecification;
-use Sitegeist\Noderobis\Domain\Specification\PropertyLabelSpecification;
-use Sitegeist\Noderobis\Domain\Specification\PropertyNameSpecification;
-use Sitegeist\Noderobis\Domain\Specification\PropertyPresetNameSpecification;
-use Sitegeist\Noderobis\Domain\Specification\PropertySpecification;
-use Sitegeist\Noderobis\Domain\Specification\PropertySpecificationCollection;
-use Sitegeist\Noderobis\Domain\Specification\PropertyTypeSpecification;
-use Sitegeist\Noderobis\Domain\Specification\TetheredNodeSpecification;
-use Sitegeist\Noderobis\Domain\Specification\TetheredNodeSpecificationCollection;
 use Sitegeist\Noderobis\Domain\Specification\TetheredNodeSpecificationFactory;
 
-/**
- * @Flow\Scope("singleton")
- */
-class NodeTypeCommandController extends CommandController
+#[Flow\Scope("singleton")]
+class NodeTypeCommandController extends AbstractCommandController
 {
-    /**
-     * @var PackageManager
-     * @Flow\Inject
-     */
-    protected $packageManager;
-
-    /**
-     * @var string|null
-     * @Flow\InjectConfiguration(path="defaultPackageKey")
-     */
-    protected $defaultPackageKey;
-
     /**
      * @var NodeTypeGenerator
      * @Flow\Inject
@@ -79,18 +45,6 @@ class NodeTypeCommandController extends CommandController
      * @Flow\Inject
      */
     protected $nodeTypeNameSpecificationFactory;
-
-    /**
-     * @var CreateNodeTypeYamlFileModificationGenerator
-     * @Flow\Inject
-     */
-    protected $createNodeTypeYamlFileModificationGenerator;
-
-    /**
-     * @var CreateFusionRendererModificationGenerator
-     * @Flow\Inject
-     */
-    protected $createFusionRendererModificationGenerator;
 
     /**
      * @phpstan-param array<int, string> $property
@@ -279,86 +233,5 @@ class NodeTypeCommandController extends CommandController
         );
 
         return $nodeTypeSpecification->withProperty($propertySpecification);
-    }
-
-
-
-    protected function determinePackage(?string $packageKey = null): FlowPackageInterface
-    {
-        if ($packageKey !== null) {
-            try {
-                $package = $this->packageManager->getPackage($packageKey);
-                if ($package instanceof FlowPackageInterface) {
-                    return $package;
-                }
-                $this->outputLine('Package %s is no Flow-Package', [$packageKey]);
-                $this->quit(1);
-                die();
-            } catch (UnknownPackageException) {
-                $this->outputLine('Package %s not found', [$packageKey]);
-                $this->quit(1);
-                die();
-            }
-        }
-
-        if ($this->defaultPackageKey !== null) {
-            try {
-                $package = $this->packageManager->getPackage($this->defaultPackageKey);
-                if ($package instanceof FlowPackageInterface) {
-                    return $package;
-                }
-                $this->outputLine('Package %s is no Flow-Package', [$packageKey]);
-                $this->quit(1);
-                die();
-            } catch (UnknownPackageException) {
-                $this->outputLine('Default Package %s not found', [$this->defaultPackageKey]);
-                $this->quit(1);
-                die();
-            }
-        }
-        $this->outputLine('No packageKey or default specified');
-        $this->quit(1);
-        die();
-    }
-
-    /**
-     * @param NodeTypeSpecification $nodeTypeSpecification
-     * @param FlowPackageInterface $package
-     * @param bool $yes
-     * @return void
-     */
-    protected function generateNodeTypeFromSpecification(NodeTypeSpecification $nodeTypeSpecification, FlowPackageInterface $package, bool $yes = false): void
-    {
-        $this->outputLine();
-        $this->outputLine($nodeTypeSpecification->__toString());
-
-        $nodeType = $this->nodeTypeGenerator->generateNodeType($nodeTypeSpecification);
-
-        $this->applyModification(
-            $yes,
-            $this->createNodeTypeYamlFileModificationGenerator->generateModification($package, $nodeType),
-            $this->createFusionRendererModificationGenerator->generateModification($package, $nodeType),
-        );
-    }
-
-    protected function applyModification(bool $yes, ModificationInterface ...$modifications): void
-    {
-        $collection = new ModificationCollection(... $modifications);
-
-        $this->outputLine();
-        $this->outputLine("The given specification will result in the following modifications");
-
-        $this->outputLine();
-        $this->outputLine($collection->getDescription());
-
-        if (!$yes && $collection->isConfirmationRequired()) {
-            $this->outputLine();
-            $this->outputLine("Confirmation is required.");
-            if ($this->output->askConfirmation("Shall we proceed (Y/n)? ") == false) {
-                $this->quit(1);
-            }
-        }
-
-        $collection->apply();
     }
 }
